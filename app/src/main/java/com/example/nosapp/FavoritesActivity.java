16f20c5f5cefe34fcb63303389a808768de78b77
cyberpapi <ahadusolomon@gmail.com>
2023-02-24 13:19:46 -0500

    Ahadu Favorites XML update

--- app/src/main/java/com/example/nosapp/FavoritesActivity.java
+++ app/src/main/java/com/example/nosapp/FavoritesActivity.java
@@ -1,13 +1,22 @@
 package com.example.nosapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FavoritesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ShowAdapter showAdapter;
+    private List<Show> showList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,25 @@ public class FavoritesActivity extends AppCompatActivity {
         randomButton();
         homeButton();
         favoritesButton();
+
+        recyclerView = findViewById(R.id.rvFavorites);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+
+        showList = new ArrayList<>();
+
+        showAdapter = new ShowAdapter(this, showList);
+        recyclerView.setAdapter(showAdapter);
+
+        loadShows();
+    }
+
+    private void loadShows() {
+
+        showList.add(new Show("Show 1", R.drawable.jimmy_neutron_logo));
+        showList.add(new Show("Show 2", R.drawable.kim_possible_logo));
+        showList.add(new Show("Show 3", R.drawable.the_fairly_oddparents_logo));
+
+        showAdapter.notifyDataSetChanged();
     }
 
     private void randomButton() {
